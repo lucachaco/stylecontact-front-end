96c2e4cee14809b4052d7e5243e21992ceb95ff4
lucachaco <luiscarlosch@gmail.com>
2015-01-14 00:19:33 -0600

    Init machine.

---
+++ conf/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+apt-get update
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
+apt-get install -y git
+
