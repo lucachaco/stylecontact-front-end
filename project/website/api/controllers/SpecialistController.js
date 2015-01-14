c28e465fe36d4f0921c629c97de6a254b82f71c1
lucachaco <luiscarlosch@gmail.com>
2015-01-14 13:56:19 -0600

    sails generate api specialist.

---
+++ project/website/api/controllers/SpecialistController.js
@@ -0,0 +1,11 @@
+/**
+ * SpecialistController
+ *
+ * @description :: Server-side logic for managing specialists
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
